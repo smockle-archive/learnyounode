dc50c11a2155d8acd0e24b1c97a5dcbfa9150beb
Clay Miller <clay@smockle.com>
2015-04-11 01:18:52 -0700

    Refactor async code.

--- dist/e5.js
+++ dist/e5.js
@@ -3,9 +3,9 @@
 /*jshint esnext: true */
 "use strict";
 
-var _createClass = (function () { function defineProperties(target, props) { for (var key in props) { var prop = props[key]; prop.configurable = true; if (prop.value) prop.writable = true; } Object.defineProperties(target, props); } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
 var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
 var babel_polyfill = require("babel/polyfill"),
     co = require("co"),
@@ -18,89 +18,71 @@ var Directory = (function () {
     this.path = path;
   }
 
-  _createClass(Directory, {
-    readdir: {
-      value: regeneratorRuntime.mark(function readdir() {
-        var _this = this;
-
-        return regeneratorRuntime.wrap(function readdir$(context$2$0) {
-          while (1) switch (context$2$0.prev = context$2$0.next) {
-            case 0:
-              context$2$0.next = 2;
-              return Directory.readdir(_this.path);
-
-            case 2:
-            case "end":
-              return context$2$0.stop();
-          }
-        }, readdir, this);
-      })
-    },
-    listByExtension: {
-      value: function listByExtension(extension) {
-        return co(regeneratorRuntime.mark(function callee$2$0() {
-          var _this = this;
-
+  _createClass(Directory, [{
+    key: "readdir",
+    value: function readdir(path) {
+      return new Promise(function (resolve, reject) {
+        fs.readdir(path, function (err, data) {
+          if (err) reject(err);else resolve(data);
+        });
+      });
+    }
+  }, {
+    key: "listByExtension",
+    value: function listByExtension(extension) {
+      return new Promise((function (resolve, reject) {
+        co(regeneratorRuntime.mark(function callee$3$0() {
           var list;
-          return regeneratorRuntime.wrap(function callee$2$0$(context$3$0) {
-            while (1) switch (context$3$0.prev = context$3$0.next) {
+          return regeneratorRuntime.wrap(function callee$3$0$(context$4$0) {
+            while (1) switch (context$4$0.prev = context$4$0.next) {
               case 0:
-                context$3$0.next = 2;
-                return _this.readdir().next().value;
+                context$4$0.next = 2;
+                return this.readdir(this.path);
 
               case 2:
-                list = context$3$0.sent;
-                return context$3$0.abrupt("return", list.filter(function (element, index, array) {
+                list = context$4$0.sent;
+
+                resolve(list.filter(function (element, index, array) {
                   return element.toLowerCase().endsWith("." + extension);
                 }));
 
               case 4:
               case "end":
-                return context$3$0.stop();
+                return context$4$0.stop();
             }
-          }, callee$2$0, this);
+          }, callee$3$0, this);
         }).bind(this))["catch"](function (err) {
-          return console.log(err.stack);
+          return reject(err);
         });
-      }
+      }).bind(this));
     }
-  }, {
-    readdir: {
-      value: function readdir(path) {
-        return function (done) {
-          fs.readdir(path, done);
-        };
-      }
-    }
-  });
+  }]);
 
   return Directory;
 })();
 
-(function () {
-  co(regeneratorRuntime.mark(function callee$1$0() {
-    var directory, list;
-    return regeneratorRuntime.wrap(function callee$1$0$(context$2$0) {
-      while (1) switch (context$2$0.prev = context$2$0.next) {
-        case 0:
-          directory = new Directory(process.argv[2]);
-          context$2$0.next = 3;
-          return Promise.resolve(directory.listByExtension(process.argv[3]));
+co(regeneratorRuntime.mark(function callee$0$0() {
+  var directory, list;
+  return regeneratorRuntime.wrap(function callee$0$0$(context$1$0) {
+    while (1) switch (context$1$0.prev = context$1$0.next) {
+      case 0:
+        directory = new Directory(process.argv[2]);
+        context$1$0.next = 3;
+        return directory.listByExtension(process.argv[3]);
 
-        case 3:
-          list = context$2$0.sent;
+      case 3:
+        list = context$1$0.sent;
 
-          list.forEach(function (currentValue, index, array) {
-            return console.log(currentValue);
-          });
+        list.forEach(function (currentValue, index, array) {
+          return console.log(currentValue);
+        });
 
-        case 5:
-        case "end":
-          return context$2$0.stop();
-      }
-    }, callee$1$0, this);
-  }))["catch"](function (err) {
-    return console.log(err.stack);
-  });
-})();
+      case 5:
+      case "end":
+        return context$1$0.stop();
+    }
+  }, callee$0$0, this);
+}))["catch"](function (err) {
+  return console.log(err.stack);
+});
 
--- e5.js
+++ e5.js
@@ -11,31 +11,30 @@ class Directory {
     this.path = path;
   }
 
-  static readdir(path) {
-    return done => {
-      fs.readdir(path, done);
-    };
-  }
-
-  *readdir() {
-    yield Directory.readdir(this.path);
+  readdir(path) {
+    return new Promise((resolve, reject) => {
+      fs.readdir(path, (err, data) => {
+        if (err) reject(err);
+        else resolve(data);
+      });
+    });
   }
 
   listByExtension(extension) {
-    return co(function* () {
-      let list = yield this.readdir().next().value;
-      return list.filter((element, index, array) =>
-        element.toLowerCase().endsWith("." + extension));
-    }.bind(this))
-    .catch(err => console.log(err.stack));
+    return new Promise(function(resolve, reject) {
+      co(function* () {
+        let list = yield this.readdir(this.path);
+        resolve(list.filter((element, index, array) =>
+          element.toLowerCase().endsWith("." + extension)));
+      }.bind(this))
+      .catch(err => reject(err));
+    }.bind(this));
   }
 }
 
-(() => {
-  co(function* () {
-    let directory = new Directory(process.argv[2]);
-    let list = yield Promise.resolve(directory.listByExtension(process.argv[3]));
-    list.forEach((currentValue, index, array) => console.log(currentValue));
-  })
-  .catch(err => console.log(err.stack));
-})();
+co(function* () {
+  let directory = new Directory(process.argv[2]);
+  let list = yield directory.listByExtension(process.argv[3]);
+  list.forEach((currentValue, index, array) => console.log(currentValue));
+})
+.catch(err => console.log(err.stack));
